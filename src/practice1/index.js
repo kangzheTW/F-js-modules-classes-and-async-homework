710256dc10ebc5b91ed9938303823030df106dd2
Zhe Kang <zkang@thoughtworks.com>
2020-07-04 13:21:06 +0800

    complete practice

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,5 @@
+import { ADDRESS, PORT, PATH, REGION } from "./constant";
 
+export const url = `${ADDRESS}:${PORT}${PATH}`;
+
+export { REGION as region };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,7 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return [data.origin, data.author, data.content];
+    });
+};
